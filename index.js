261f6b5af9220922448bce69291deb652b315d61
Emma Lynn <emmalynn@Emmas-Mac-mini.local>
2021-02-01 11:05:25 -0700

    configure server code for axton

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+require('dotenv/config')
+
+const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors());
+var port = process.env.PORT || 5001;
+
+app.get('/', (req, res) => [
+    res.status(200).send('Root Route')
+])
+app.post('/contact', (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+  var mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: 'emma@mcfaddengavender.com',
+    subject: `New email from ${req.body.name}`,
+    html: `<h3>Subject: ${req.body.subject}</h3><br/><h3>Email: ${req.body.email}</h3><br/><h3>Phone: ${req.body.phone}</h3><br/><p><strong>Message: </strong>${req.body.message}</p>`
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+})
+app.listen(port, () => {
+    console.log(`Emma Contact server is listening on port ${port}`)
+})
